cc03722608d40863acaa2db8a0a33c27915fef78
Gabriel <gabriel.gracilianomb@gmail.com>
2026-08-30 11:28:02 -0300

    Optimize images with vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import { ViteMinifyPlugin } from "vite-plugin-minify";
 
 export default defineConfig({
-  plugins: [ViteMinifyPlugin({})],
+  plugins: [
+    ViteMinifyPlugin({}),
+    ViteImageOptimizer({
+      jpeg: {
+        quality: 60,
+      },
+    }),
+  ],
 });
